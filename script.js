6189a6e73b26ec330bbad2be7e115a3c63a62ebe
Kushal <27188278+kuushal@users.noreply.github.com>
2023-02-20 09:58:19 +0530

    finished creating static size grid with random color on hover

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
-const canvas = document.querySelector('#myCanvas');
+const container = document.querySelector('.container');
+drawGrid(16);
+const divs = document.querySelectorAll('.square');
+divs.forEach(div => div.addEventListener('mouseover', changeBackgroundColor));
 
-if (canvas.getContext) {
-    const ctx = canvas.getContext('2d');
-    let xCoordinate = 2;
-    let yCoordinate = 2;
-    while (yCoordinate <= canvas.height) {
-        if (xCoordinate >= canvas.width) {
-            xCoordinate = 2;
-            yCoordinate += 20;
-        }
-        ctx.strokeRect(xCoordinate, yCoordinate, 16, 16);
-        xCoordinate += 20;
-
-    }
-    console.log(xCoordinate);
-    console.log(yCoordinate);
+function changeBackgroundColor(e) {
+    let eventType = e.type;
+    const element = e.target;
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    element.style.backgroundColor = `#${randomColor}`;
 }
+
+function drawGrid(size) {
+    for (let i = 0; i < size; i++) {
+        const subContainer = document.createElement('div');
+        subContainer.classList.add('sub-container');
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            subContainer.appendChild(square);
+            container.appendChild(subContainer);
+        }
+    }
+}
+
+
+

584a407cb01ea54aed27e23067c894036106d102
Kushal <27188278+kuushal@users.noreply.github.com>
2023-02-20 10:51:43 +0530

    added dynamic grid size

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const container = document.querySelector('.container');
 drawGrid(16);
-const divs = document.querySelectorAll('.square');
-divs.forEach(div => div.addEventListener('mouseover', changeBackgroundColor));
 
-function changeBackgroundColor(e) {
+
+const resetButton = document.querySelector('.reset');
+resetButton.addEventListener('click', resetGrid);
+
+const setSizeButton = document.querySelector('.set-size');
+setSizeButton.addEventListener('click', setGridSize);
+
+
+function changeBackgroundColorRandom(e) {
     let eventType = e.type;
     const element = e.target;
     let randomColor = Math.floor(Math.random() * 16777215).toString(16);
     element.style.backgroundColor = `#${randomColor}`;
 }
+
 
 function drawGrid(size) {
     for (let i = 0; i < size; i++) {
@@ -21,7 +28,26 @@ function drawGrid(size) {
             container.appendChild(subContainer);
         }
     }
+
+    const divs = document.querySelectorAll('.square');
+    divs.forEach(div => div.addEventListener('mouseover', changeBackgroundColorRandom));
 }
 
+function resetGrid() {
+    const divs = document.querySelectorAll('.square');
+    divs.forEach(div => div.style.backgroundColor = '#fff');
+}
+
+function setGridSize(e) {
+    let error = document.getElementById('error');
+    let size = document.getElementById('size').value;
+    if (size >= 0 && size <= 100) {
+        error.textContent = '';
+        container.replaceChildren();
+        drawGrid(size);
+    } else {
+        error.textContent = 'Please enter value between 0 & 100';
+    }
+}
 
 
